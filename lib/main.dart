b42ced31420a2ed3ccd64c1ae6bd56bef049cf3a
Hawa S <hmsylla10@gmail.com>
2021-10-24 15:39:51 -0400

    first

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Recipes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.white,
+          textTheme: TextTheme(
+            bodyText2: TextStyle(color: Colors.white),
+          )),
+      home: HomePage(),
+    );
+  }
+}
